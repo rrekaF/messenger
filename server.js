10e414a26422ba09f1811a8f912e8f36e2bb641f
Patryk Wojnarowski <pewuka2005@wp.pl>
2023-10-14 01:51:21 +0200

    Messages now get added to a db

--- server.js
+++ server.js
@@ -2,16 +2,53 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  connectionStateRecovery: {},
-});
+const { MongoClient } = require("mongodb");
+async function main() {
+  const pwd = encodeURIComponent("2Wr&p%S4");
+  const uri = "mongodb+srv://patrykawojnarowski:" + pwd + "@message-app.ubyzplz.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+  const dbName = "message-app";
+  const collectionName = "messages";
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+  const database = await client.db(dbName);
+  console.log("connected to db");
+  collection = database.collection(collectionName);
+  console.log("connected to collection");
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000.");
-});
+  async function insertMessage(message) {
+    try {
+      const insertResult = collection.insertOne(message).then(() => {
+        console.log("inserted successfully");
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+  });
+
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
+
+  io.on("connection", (socket) => {
+    console.log("User connected");
+    socket.on("new message", (message) => {
+      insertMessage(message);
+      io.emit("new message", message);
+    });
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+    });
+  });
+
+  server.listen(3000, () => {
+    console.log("Server running on port 3000.");
+  });
+  await client.close();
+}
+main();
